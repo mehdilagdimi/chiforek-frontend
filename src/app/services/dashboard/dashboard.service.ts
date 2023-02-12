7b83a40b59ca394de6dd9b04b494c58565d75563
mehdilagdimi <lagdimi.mehdi@gmail.com>
2023-02-12 21:51:59 +0100

    updating dashboard

--- src/app/services/dashboard/dashboard.service.ts
+++ src/app/services/dashboard/dashboard.service.ts
@@ -1,3 +1,5 @@
+import { API_URL } from './../../../config/api.constants';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import {BehaviorSubject} from "rxjs/internal/BehaviorSubject";
 import {Observable} from "rxjs/internal/Observable";
@@ -7,227 +9,18 @@ import {Observer, Subject} from "rxjs";
   providedIn: 'root'
 })
 export class DashboardService {
-  data: Array<Object> = [
-    {
-      name: "bla bla",
-      description: "tza tza",
-      profile: "k3a k3a",
-      taha: "taha1"
-    },
-    {
-      name: "bla bla",
-      description: "tza tza",
-      profile: "k3a k3a",
-      taha: "taha2"
-    },
-    {
-      name: "bla bla",
-      description: "tza tza",
-      profile: "k3a k3a",
-      taha: "taha3"
-    },
-    {
-      name: "bla bla",
-      description: "tza tza",
-      profile: "k3a k3a",
-      taha: "taha4"
-    },
-    {
-      name: "bla bla",
-      description: "tza tza",
-      profile: "k3a k3a",
-      taha: "taha1"
-    },
-    {
-      name: "bla bla",
-      description: "tza tza",
-      profile: "k3a k3a",
-      taha: "taha2"
-    },
-    {
-      name: "bla bla",
-      description: "tza tza",
-      profile: "k3a k3a",
-      taha: "taha3"
-    },
-    {
-      name: "bla bla",
-      description: "tza tza",
-      profile: "k3a k3a",
-      taha: "taha4"
-    },
-    {
-      name: "bla bla",
-      description: "tza tza",
-      profile: "k3a k3a",
-      taha: "taha1"
-    },
-    {
-      name: "bla bla",
-      description: "tza tza",
-      profile: "k3a k3a",
-      taha: "taha2"
-    },
-    {
-      name: "bla bla",
-      description: "tza tza",
-      profile: "k3a k3a",
-      taha: "taha3"
-    },
-    {
-      name: "bla bla",
-      description: "tza tza",
-      profile: "k3a k3a",
-      taha: "taha4"
-    },
-    {
-      name: "bla bla",
-      description: "tza tza",
-      profile: "k3a k3a",
-      taha: "taha1"
-    },
-    {
-      name: "bla bla",
-      description: "tza tza",
-      profile: "k3a k3a",
-      taha: "taha2"
-    },
-    {
-      name: "bla bla",
-      description: "tza tza",
-      profile: "k3a k3a",
-      taha: "taha3"
-    },
-    {
-      name: "bla bla",
-      description: "tza tza",
-      profile: "k3a k3a",
-      taha: "taha4"
-    },
-    {
-      name: "bla bla",
-      description: "tza tza",
-      profile: "k3a k3a",
-      taha: "taha1"
-    },
-    {
-      name: "bla bla",
-      description: "tza tza",
-      profile: "k3a k3a",
-      taha: "taha2"
-    },
-    {
-      name: "bla bla",
-      description: "tza tza",
-      profile: "k3a k3a",
-      taha: "taha3"
-    },
-    {
-      name: "bla bla",
-      description: "tza tza",
-      profile: "k3a k3a",
-      taha: "taha4"
-    },
-    {
-      name: "bla bla",
-      description: "tza tza",
-      profile: "k3a k3a",
-      taha: "taha1"
-    },
-    {
-      name: "bla bla",
-      description: "tza tza",
-      profile: "k3a k3a",
-      taha: "taha2"
-    },
-    {
-      name: "bla bla",
-      description: "tza tza",
-      profile: "k3a k3a",
-      taha: "taha3"
-    },
-    {
-      name: "bla bla",
-      description: "tza tza",
-      profile: "k3a k3a",
-      taha: "taha4"
-    },
-    {
-      name: "bla bla",
-      description: "tza tza",
-      profile: "k3a k3a",
-      taha: "taha1"
-    },
-    {
-      name: "bla bla",
-      description: "tza tza",
-      profile: "k3a k3a",
-      taha: "taha2"
-    },
-    {
-      name: "bla bla",
-      description: "tza tza",
-      profile: "k3a k3a",
-      taha: "taha3"
-    },
-    {
-      name: "bla bla",
-      description: "tza tza",
-      profile: "k3a k3a",
-      taha: "taha4"
-    },
-    {
-      name: "bla bla",
-      description: "tza tza",
-      profile: "k3a k3a",
-      taha: "taha1"
-    },
-    {
-      name: "bla bla",
-      description: "tza tza",
-      profile: "k3a k3a",
-      taha: "taha2"
-    },
-    {
-      name: "bla bla",
-      description: "tza tza",
-      profile: "k3a k3a",
-      taha: "taha3"
-    },
-    {
-      name: "bla bla",
-      description: "tza tza",
-      profile: "k3a k3a",
-      taha: "taha4"
-    },
-    {
-      name: "bla bla",
-      description: "tza tza",
-      profile: "k3a k3a",
-      taha: "taha1"
-    },
-    {
-      name: "bla bla",
-      description: "tza tza",
-      profile: "k3a k3a",
-      taha: "taha2"
-    },
-    {
-      name: "bla bla",
-      description: "tza tza",
-      profile: "k3a k3a",
-      taha: "taha3"
-    },
-    {
-      name: "bla bla",
-      description: "tza tza",
-      profile: "k3a k3a",
-      taha: "taha4"
-    }
-  ]
+  data: Array<Object> = [{}]
   dataChange: Subject<Array<Object>> = new Subject<Array<Object>>();
 
-  constructor() {
+  constructor(private httpClient: HttpClient) {
+
+    this.httpClient.get(`${API_URL}/reservations`).subscribe({
+      next: (response: any) => {
+        this.dataChange.next(response.data.data)
+      }
+    })
+
+
     this.dataChange.subscribe((value => {
       this.data = value
     }))
